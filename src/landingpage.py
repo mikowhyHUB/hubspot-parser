22affce276ffe52c9a834aa3963468aa35f574f3
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-10-04 20:27:29 +0200

    changed: sort for hours-days instead of just days

--- src/landingpage.py
+++ src/landingpage.py
@@ -2,35 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
 # MVP
+
+
+def parse_blog_post(url):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    date_published = None
+
+    script_tag = soup.find("script", {"type": "application/ld+json"})
+    if script_tag:
+        script_data = script_tag.text
+        try:
+            date_published = datetime.fromisoformat(
+                script_data.split('"datePublished": "')[1].split('"')[0]
+            )
+        except IndexError:
+            pass
+
+    return date_published
+
+
 def latest_hubspot_articles(url, articles_num):
     response = requests.get(url)
-    response.raise_for_status()
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     article_sections = soup.find_all("li", class_="blog-categories-post -visible")
 
-    articles = []
-    for section in article_sections:
-        link = section.find("h3", class_="blog-categories-card-title").find("a")["href"]
-        date_str = (
-            section.find("div", class_="blog-categories-card-footer")
-            .find_all("p")[1]
-            .text.strip()
-        )
+    articles = [
+        section.find("h3", class_="blog-categories-card-title").find("a")["href"]
+        for section in article_sections
+    ]
 
-        date_obj = datetime.strptime(date_str, "%m/%d/%y")
+    articles.sort(reverse=True)
 
-        articles.append(
-            {"date": date_obj, "link": link}
-        )
-
-    articles.sort(key=lambda x: x["date"], reverse=True)
-
-    for article in articles[:articles_num]:
-        print(article['link'])
+    for article_link in articles[:articles_num]:
+        date_published = parse_blog_post(article_link)
+        print(article_link, date_published)
 
 
 if __name__ == "__main__":
